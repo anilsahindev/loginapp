9841343381878d668618877f9defecc86f578f6b
anil sahin <anil.sahin@epigra.com>
2018-10-15 01:02:10 +0300

    watcher add

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,25 @@ var sass = require('gulp-sass');
 var minifyCSS = require('gulp-csso');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+gulp.task('watch', function() {
+
+    browserSync.init({
+        server: "./public"
+    });
+  
+    gulp.watch("assets/scss/*.scss", ['sass']);
+    gulp.watch("views/*.handlebars").on('change', browserSync.reload);
+  });
+  
+  gulp.task('sass', function() {
+    return gulp.src("assets/scss/app.scss")
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest("public/css"))
+      .pipe(browserSync.stream());
+  });
+  
+  gulp.task('default', ['sass', 'serve']);
 
 gulp.task('css', function(){
     return gulp.src([
@@ -20,3 +39,5 @@ gulp.task('css', function(){
       .pipe(sourcemaps.write())
       .pipe(gulp.dest('public/js'))
   });
+
+
